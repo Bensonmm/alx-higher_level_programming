1d3b6ef266cb8c97642c73a4dde57ec78a01417a
Bensonmm <27972639+Bensonmm@users.noreply.github.com>
2023-08-21 15:40:36 -0700

    0x18-dyanmic

--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -1,20 +1,20 @@
-#include "main.h"	
-	/**
-	 * _strncat - function concatenate two strings
-	 * using at most n bytes from src
-	 * @dest: entered value
-	 * @src: entered value
-	 * @n: entered value
-	 *
-	 * Return: dest
-	 */
-	char *_strncat(char *dest, char *src, int n)
-	{
-	        	int a;
-	        	int b;
-	 
-	        	a = 0;
-	        	while (dest[a] != '\0')
+#include "main.h"
+/**
+* _strncat - function concatenate two strings
+* using at most n bytes from src
+* @dest: entered value
+* @src: entered value
+* @n: entered value
+*
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
 	        	{
 	                    	a++;
 	        	}
